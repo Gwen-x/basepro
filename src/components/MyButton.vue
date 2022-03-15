c3629c31ee0811c3623fba9187df3f8ddf1d1f79
Gwen-x <2764014401@qq.com>
2022-03-15 22:13:22 +0800

    first

---
+++ src/components/MyButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>111</h2>
+        <button @click="count++">{{count}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    data(){
+        return {
+            count:0
+        }
+    }
+}
+</script>
+
+<style  scoped>
+    h2{
+        color: hotpink;
+    }
+</style>
